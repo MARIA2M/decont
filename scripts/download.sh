0f55c1e004560f112d6ebbe470a6f5b3b5fc3b01
MARIA2M <mariamoralesmar@hotmail.com>
2019-11-18 19:02:59 +0100

    Descarga de archivos fastq

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,2 +1,16 @@
-# This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument, 
+# This script should download the file specified in the first argument ($1), place it in the directory specified in the second argument,
 # and *optionally* uncompress the downloaded file with gunzip if the third argument contains the word "yes".
+
+# Nombre del archivo que queremos descargar
+fasta_file=$(basename $1)
+
+echo "Descargango archivo: "$fasta_file" en la carpeta "$2
+echo "-----------------------------------------------------\n"
+wget  -P $2 $1
+
+if  [ "$3" == "yes" ]
+then
+  echo "Descomprimiendo archivo"$fasta_file
+  echo "-----------------------------------------------------\n"
+  gunzip $2/$fasta_file
+fi
